89d95127a223bdc3ae8f8b54cb510741055ed463
Rampant91 <rampant91@gmail.com>
2022-07-22 14:21:04 +0300

    Add project files.

---
+++ OrderBook/DBRealization/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderBook.DBRealization
+{
+    public class DataContext : DbContext
+    {
+        private readonly string ConnectionString = @"";
+
+    }
+}
---
+++ OrderBook/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderBook.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+
+        public int Price { get; set; }
+
+        public int Amount { get; set; }
+
+        public string Discription { get; set; }
+    }
+}
